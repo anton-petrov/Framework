a01aabc2885c9967cb09775eb60901656f960cb4
Anton Petrov <petrov.anton@live.com>
2016-06-09 20:40:12 +0300

    123

--- src/edu/petrov/framework/Main.java
+++ src/edu/petrov/framework/Main.java
@@ -1,32 +1,82 @@
 package edu.petrov.framework;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 class ExecutorImpl<T> implements Executor<T> {
 
+    private static class TaskAndValidator<T> {
+
+        Task<? extends T> task;
+
+        public TaskAndValidator(Task<? extends T> task, Validator<T> validator) {
+            this.task = task;
+            this.validator = validator;
+        }
+
+        Validator<T> validator;
+
+        public Task<? extends T> getTask() {
+            return task;
+        }
+
+        public void setTask(Task<? extends T> task) {
+            this.task = task;
+        }
+
+        public Validator<T> getValidator() {
+            return validator;
+        }
+
+        public void setValidator(Validator<T> validator) {
+            this.validator = validator;
+        }
+    }
+
+    private List<TaskAndValidator<T>> taskAndValidatorList = new LinkedList<TaskAndValidator<T>>();
+
     @Override
     public void addTask(Task<? extends T> task) {
-
+        taskAndValidatorList.add(new TaskAndValidator<>(task, null));
     }
 
     @Override
     public void addTask(Task<? extends T> task, Validator<T> validator) {
-
+        taskAndValidatorList.add(new TaskAndValidator<>(task, validator));
     }
 
     @Override
     public void execute() {
-
+        for (TaskAndValidator<T> taskAndValidator : taskAndValidatorList) {
+            taskAndValidator.getTask().execute();
+        }
     }
 
     @Override
     public List<T> getValidResults() {
-        return null;
+        List<T> result = new LinkedList<T>();
+        for (TaskAndValidator<T> taskAndValidator : taskAndValidatorList) {
+            if (taskAndValidator.getValidator() != null &&
+                    taskAndValidator.getValidator().isValid(taskAndValidator.getTask().getResult())) {
+
+                result.add(taskAndValidator.getTask().getResult());
+            }
+        }
+        return result;
     }
 
     @Override
     public List<T> getInvalidResults() {
-        return null;
+        List<T> result = new LinkedList<T>();
+        for (TaskAndValidator<T> taskAndValidator : taskAndValidatorList) {
+            if (taskAndValidator.getValidator() != null &&
+                    !taskAndValidator.getValidator().isValid(taskAndValidator.getTask().getResult())) {
+
+                result.add(taskAndValidator.getTask().getResult());
+            }
+        }
+        return result;
     }
 
 }
@@ -41,14 +91,20 @@ class NumberValidator<T extends Number> implements Validator<T> {
 
 class NumberTask<T extends Number> implements Task<T> {
 
+    private T value;
+
+    public NumberTask(T value) {
+        this.value = value;
+    }
+
     @Override
     public void execute() {
-        System.out.println("NumberTask!");
+        System.out.println("NumberTask! Value => " + value);
     }
 
     @Override
     public T getResult() {
-        return null;
+        return value;
     }
 }
 
@@ -60,7 +116,7 @@ public class Main {
         for (Task<Integer> intTask : intTasks) {
             numberExecutor.addTask(intTask);
         }
-        numberExecutor.addTask(new NumberTask<Long>(), new NumberValidator<>());
+        numberExecutor.addTask(new NumberTask<>(10L), new NumberValidator<>());
 
         numberExecutor.execute();
 
@@ -76,6 +132,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        List<Task<Integer>> tasks = new LinkedList<>();
+        tasks.add(new NumberTask<>(1));
+        tasks.add(new NumberTask<>(2));
+        tasks.add(new NumberTask<>(3));
+        test(tasks);
     }
 }
